f2518ac3b39d8ffcf5bed7faeca60e15d6d66420
Steven C <stevenc.github@gmail.com>
2019-04-15 17:22:01 -0400

    Changes to the user input prompt, allow for the auto_rotate input variable.

--- gopro_main.py
+++ gopro_main.py
@@ -34,34 +34,41 @@ def print_intro():
 
     output_path = str(input("Output path: "))
 
-    down_convert = str(input("Downconvert the GoPro files [y/N]: "))
-
-    auto_rotate = str(input("Allow FFMPEG to auto rotate video? [Y/n]: "))
-
     while True:
-        if down_convert[0].lower() == 'y':
-            down_convert = True
-            break
-        elif down_convert[0].lower() == 'n' or None:
-            down_convert = False
-            break
-        else:
-            print(f"{down_convert} is not a valid choice.")
-            down_convert = str(input("Please select Yes or No [y/N] "))
+        try:
+            down_convert = str(input("Downconvert the GoPro files [y/N]: "))
+
+            if down_convert[0].lower() == 'y':
+                down_convert = True
+                break
+            elif down_convert[0].lower() == 'n':
+                down_convert = False
+                break
+            else:
+                print(f"{down_convert} is not a valid choice.")
+                down_convert = str(input("Please select Yes or No [y/N] "))
+                continue
+        except ValueError:
+            print("ValueError, try again")
             continue
 
     while True:
-        if auto_rotate[0].lower() == 'y' or None:
-            auto_rotate = True
-            break
-        elif auto_rotate[0].lower() == 'n':
-            auto_rotate = False
-            break
-        else:
-            print(f"{auto_rotate} is not a valid choice.")
-            auto_rotate = str(input("Please select Yes or No [Y/n]: "))
+        try:
+            auto_rotate = str(input("Allow FFMPEG to auto-rotate video? [Y/n]: "))
+            if auto_rotate[0].lower() == 'y':
+                auto_rotate = True
+                break
+            elif auto_rotate[0].lower() == 'n':
+                auto_rotate = False
+                break
+            else:
+                print(f"{auto_rotate} is not a valid choice.")
+                continue
+        except ValueError:
+            print("ValueError, try again")
             continue
 
+    print([source_path, output_path, down_convert, auto_rotate])
     return [source_path, output_path, down_convert, auto_rotate]
 
 def gopro_main():
@@ -69,19 +76,20 @@ def gopro_main():
     Get the user input and pass it into the concat and/or downconvert
     functions.
     '''
-    gp_vars = gp.print_intro()
+    gp_vars = print_intro()
 
     source_path = gp_vars[0]
     output_path = gp_vars[1]
     down_convert = gp_vars[2]
+    auto_rotate = gp_vars[3]
 
     start_message = f"\
     ================================================================\n \
         Starting the GoPro concat with these values : \n \
         Source Path:   {str(source_path)} \n \
         Output Path:   {str(output_path)} \n \
-        DownConvert:    {str(down_convert)} \n \
-        Auto Rotation:    {str(down_convert)} \n \
+        DownConvert:   {str(down_convert)} \n \
+        Auto Rotate:   {str(auto_rotate)} \n \
     ===========================================================\n "
 
     print(start_message)
@@ -92,15 +100,15 @@ def gopro_main():
     if down_convert == False:
 
         for gprkey in gprkey_list:
-            gp.ffmpeg_concat(gopro_dict, source_path, output_path)
+            gp.ffmpeg_concat(gprkey, gopro_dict, source_path, output_path, auto_rotate)
             print('Processing concat now...')
 
     else:
 
         for gprkey in gprkey_list:
-            ffmpeg_downconvert = gp.ffmpeg_downconvert(gprkey, gopro_dict, source_path, output_path)
+            ffmpeg_downconvert = gp.ffmpeg_downconvert(gprkey, gopro_dict, source_path, output_path, auto_rotate)
 
-        print( "Processing downconvert now...")
+        print("Processing downconvert now...")
 
     print('FFMPEG process complete.')
 
