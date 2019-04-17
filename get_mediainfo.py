8403999cef55fd5d4d60b15718ac8f8d57cf280f
Steven C <stevenc.github@gmail.com>
2019-04-17 17:02:23 -0400

    Minor change

--- get_mediainfo.py
+++ get_mediainfo.py
@@ -54,7 +54,6 @@ def get_mediainfo(source_path, gprkey):
             media_dict.update(a_sampling_rate = track.sampling_rate)
             media_dict.update(a_compression_mode = track.compression_mode)
 
-
     # print(media_dict)
 
     return media_dict
